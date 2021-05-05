f52a129ad4905f61c285f0546c755ca21a6ed432
fawad60 <fawadmalik60@gmail.com>
2021-05-05 23:37:21 +0500

    added api

--- src/App.js
+++ src/App.js
@@ -6,12 +6,37 @@ import SignupForm from "./components/SignUp";
 import LoginForm from "./components/login";
 import { Navigation } from "./components/navigation";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
 import "./App.css";
 import "./components/temperature.css";
 import Turbity from "./components/turbity";
 
 function App() {
+  const SearchRecord = () => {
+    var SearchAPIURL = "http://localhost/fyp/temp.php";
+    var headers = {
+      Accept: "application/json",
+      "Content-Type": "application.json",
+    };
+
+    var Data = {};
+
+    fetch(SearchAPIURL, {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify(Data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        // this.setState({ temp: response[0].Temperature });
+      })
+      .catch((error) => {
+        alert("Error" + error);
+      });
+  };
+
+  console.log(SearchRecord());
+
   return (
     <div className="App">
       <Router>
---
+++ src/fyp.js
@@ -0,0 +1,57 @@
+
+
+import React, { Component } from "react";
+import { StyleSheet, TextInput, View, Button } from "react-native";
+
+export default class fyp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { temp: "" };
+    this.SearchRecord();
+    setTimeout(this.SearchRecord, 4000);
+  }
+  SearchRecord = () => {
+    var SearchAPIURL = "http://192.168.8.100/fyp/temp.php";
+    var headers = {
+      Accept: "application/json",
+      "Content-Type": "application.json",
+    };
+
+    var Data = {};
+
+    fetch(SearchAPIURL, {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify(Data),
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        this.setState({ temp: response[0].Temperature });
+      })
+      .catch((error) => {
+        alert("Error" + error);
+      });
+  };
+  render() {
+    return (
+      <View style={styles.ViewStyle}>
+        <TextInput style={styles.txt} value={this.state.temp} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  ViewStyle: {
+    flex: 1,
+    padding: 20,
+    marginTop: 10,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  txt: {
+    borderBottomWidth: 1,
+    borderBottomColor: "red",
+    marginBottom: 20,
+  },
+});
